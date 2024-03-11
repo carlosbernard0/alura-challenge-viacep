6b17f23dabe839d00a3abe4f51cc372fe1357572
carlos <carlosbernardo774@gmail.com>
2024-03-11 16:41:18 -0300

    finish challange

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import com.google.gson.JsonSyntaxException;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        ConsultarCep consultarCep = new ConsultarCep();
+
+        System.out.println("Digite um cep para busca:");
+        String cep = input.nextLine();
+
+        try {
+            Endereco novoEndereco = consultarCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+
+            GerarArquivo gerarArquivo = new GerarArquivo();
+            gerarArquivo.salvaJson(novoEndereco);
+        }catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
     }
 }
